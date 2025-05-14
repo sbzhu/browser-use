4b2c5a702b15ce722f8011bf65276c0a7d0faf71
朱胜本 <zhushengben@zhushengbendeMac-mini.local>
2025-05-14 15:21:22 +0800

    优化提示词

--- examples/models/deepseek.py
+++ examples/models/deepseek.py
@@ -77,7 +77,9 @@ async def run_search(theme):
 			'click_element_by_index text_content=生成 PPT1\n'
 			'wait seconds=30, description=等待PPT生成\n'
 			'click_element_by_index text_content=下 载\n'
-			'done'
+			'done\n'
+			'\n'
+			'你可以根据历史经验一次输出尽可能多的action.'
 		),
 		llm=llm,
 
